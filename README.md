a194932045e6816791ddc2ee816603e20bfa6e67
Hector Sanjuan <code@hector.link>
2023-02-03 17:29:26 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # carls
+
 Display roots and CIDs in a CAR file.
+
+## Usage
+
+```sh
+go install github.com/hsanjuan/carls@latest
+carls file.car
+# <lists block CIDs in the CAR file>
+carls -roots file.car
+# <lists root CIDs in the CAR file>
+```
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ipld/go-car"
+)
+
+var rootsFlag = flag.Bool("roots", false, "Display CAR roots")
+
+func printfErr(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args)
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		printfErr("Usage: ./carls file.car")
+	}
+
+	f, err := os.Open(flag.Args()[1])
+	if err != nil {
+		printfErr(err.Error())
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	if *rootsFlag {
+
+		buf := bufio.NewReader(f)
+		h, err := car.ReadHeader(buf)
+		if err != nil {
+			printfErr(err.Error())
+			os.Exit(1)
+		}
+		for _, root := range h.Roots {
+			fmt.Println(root)
+		}
+		return
+	}
+
+	rdr, err := car.NewCarReader(f)
+	if err != nil {
+		printfErr(err.Error())
+		os.Exit(1)
+	}
+
+	for {
+		b, err := rdr.Next()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			printfErr(err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(b.Cid())
+	}
+}
